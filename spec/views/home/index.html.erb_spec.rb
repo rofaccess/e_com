600fc36c9dc4e18835e41962fed1a5ffe8045385
Rodrigo Fernandez <rofaccess@gmail.com>
2024-10-09 19:21:34 -0300

    Show products in home view. Show product buttons according user

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", :type => :view do
+  fixtures :products
+
+  before(:each) do
+    @products = assign(:products, Product.all)
+    allow(view).to receive(:current_user).and_return(nil)
+  end
+
   it "renders empty page" do
     render
-    assert_select "h1", :text => "Welcome", :count => 1
+    assert_select "span", :text => "Comprar", :count => 2
   end
 end
