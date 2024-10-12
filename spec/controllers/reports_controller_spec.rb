e32d13b7c4f5064095e6c37e3adb81d77cb293ba
Rodrigo Fernandez <rofaccess@gmail.com>
2024-10-11 22:16:05 -0300

    Update files and folders permission

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ReportsController, :type => :controller do
+  fixtures :users
+
+  let(:admin) {
+    users(:admin)
+  }
+
+  before do
+    sign_in admin
+  end
 
   describe "GET #index" do
     it "returns http success" do
